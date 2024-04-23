5ed7b877a9cfeb7fda4592e6e676452d3564ec21
Rubhini Anand <144896851+RubhiniAnand@users.noreply.github.com>
2024-04-23 13:52:36 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+# Create your models here.
+class insModel(models.Model):
+
+    age=models.FloatField()
+    bmi=models.FloatField()
+    children=models.FloatField()
+    sex_male=models.FloatField()
+    smoker_yes=models.FloatField()
